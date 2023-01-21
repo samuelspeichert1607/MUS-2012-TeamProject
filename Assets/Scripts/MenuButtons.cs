6ded0826f65106d8b99388222b91d3d4d0a0b162
Samuel Speichert <samuelspeichert1607@gmail.com>
2023-01-21 10:01:01 -0500

    first commit

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+	[SerializeField]
+	private string sceneName;
+	
+    public void Play(){
+		SceneManager.LoadScene(sceneName);
+	}
+}
