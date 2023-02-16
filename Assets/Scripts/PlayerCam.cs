cdbdeba15a0219b4fc3d4ee6302eefcdfe12a052
Samuel Speichert <60676766+samuelspeichert1607@users.noreply.github.com>
2023-02-16 11:20:38 -0500

    Test branch (#4)
    * test pour le fun
    
    * Update lol.txt
    
    * lvl_henri_gagnon et maincharacter ajoutés

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    [SerializeField] private Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
