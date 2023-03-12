092ff41928a561712486192576b0073323a72a4c
Samuel Speichert <samuelspeichert1607@gmail.com>
2023-03-12 19:32:06 -0400

    Intégration des modifications à Charles

---
+++ Assets/WrapToLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WrapToLevel : MonoBehaviour
+{
+    [SerializeField]
+    private string LevelToWrap;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(LevelToWrap, LoadSceneMode.Additive);
+            Destroy(collision.gameObject);
+        }
+    }
+}
